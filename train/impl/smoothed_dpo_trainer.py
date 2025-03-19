d1ec4f8af770c2434964a293e41ef0cd8e8b725f
Piotr Kaminski <piotrus.kaminski@gmail.com>
2025-03-19 23:57:08 +0100

    fix naming convention overlap

--- train/impl/smoothed_dpo_trainer.py
+++ train/impl/smoothed_dpo_trainer.py
@@ -1120,7 +1120,7 @@ class SmoothedDPOTrainer(Trainer):
             return (loss, metrics)
         return loss
 
-    def get_batch_samples(self, model, batch: Dict[str, torch.LongTensor]) -> Tuple[str, str]:
+    def get_batch_samples_trl(self, model, batch: Dict[str, torch.LongTensor]) -> Tuple[str, str]:
         """Generate samples from the model and reference model for the given batch of inputs."""
 
         # If one uses `generate_during_eval` with peft + bf16, we need to explicitly call generate with
@@ -1236,7 +1236,7 @@ class SmoothedDPOTrainer(Trainer):
             random_batch = self.data_collator(random_batch_dataset)
             random_batch = self._prepare_inputs(random_batch)
 
-            policy_output_decoded, ref_output_decoded = self.get_batch_samples(self.model, random_batch)
+            policy_output_decoded, ref_output_decoded = self.get_batch_samples_trl(self.model, random_batch)
 
             self.log(
                 {
